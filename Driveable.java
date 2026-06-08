321379f017535ddf2771764b1c159199b1ba6770
KHOR YONG YING <167044447+Kyongying@users.noreply.github.com>
2026-06-08 21:40:43 +0800

    Update Driveable.java

--- Driveable.java
+++ Driveable.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package soc_298738;
 
 /**
@@ -10,8 +6,6 @@ package soc_298738;
  */
 
 public interface Driveable {
-    //private String name;
-    //private String license;
     
 public void drive();
     
